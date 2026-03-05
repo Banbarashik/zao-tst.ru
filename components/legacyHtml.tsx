8af3addcf37876df5521fdf2384f1d1b9d316411
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-05 17:32:20 +0400

    create LegacyHtml component

---
+++ components/legacyHtml.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+export default function LegacyHtml({ path }: { path: string }) {
+  const [html, setHtml] = useState("");
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    fetch(path)
+      .then((res) => res.text())
+      .then((text) => setHtml(text));
+  }, [path]);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const scripts = containerRef.current.querySelectorAll("script");
+
+    scripts.forEach((script) => {
+      const newScript = document.createElement("script");
+
+      // копируем атрибуты
+      Array.from(script.attributes).forEach((attr) => {
+        newScript.setAttribute(attr.name, attr.value);
+      });
+
+      newScript.textContent = script.textContent;
+
+      // заменяем старый script
+      script.parentNode?.replaceChild(newScript, script);
+    });
+  }, [html]);
+
+  return <div ref={containerRef} dangerouslySetInnerHTML={{ __html: html }} />;
+}
