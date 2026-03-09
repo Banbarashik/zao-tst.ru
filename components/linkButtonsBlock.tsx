58298d406454611c75185fa17ce7a2c7a03ff7d9
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-09 15:52:47 +0400

    call reachGoal if btn has goal

--- components/linkButtonsBlock.tsx
+++ components/linkButtonsBlock.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
+import useYandexMetrika from "@/hooks/useYandexMetrika";
+
 import { Button } from "@/components/ui/button";
 
 export default function LinkButtonsBlock({ buttons }) {
+  const { reachGoal } = useYandexMetrika();
+
   const buttonsAmount = buttons.length;
 
   return (
@@ -18,7 +24,8 @@ export default function LinkButtonsBlock({ buttons }) {
         <Button
           key={btn.name}
           asChild
-          className="shrink-1 basis-full rounded-none border border-[#723910] bg-gray-200 text-black uppercase"
+          className="shrink basis-full rounded-none border border-[#723910] bg-gray-200 text-black uppercase"
+          onClick={btn.goal && (() => reachGoal(btn.goal))}
         >
           <Link href={btn.url} target={btn.openNewTab ? "_blank" : "_self"}>
             {btn.name}
