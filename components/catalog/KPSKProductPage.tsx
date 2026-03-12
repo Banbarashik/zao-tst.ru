d76933b8da7279fad6fb77cba2f9a829a175f599
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-12 14:53:51 +0400

    add table

--- components/catalog/KPSKProductPage.tsx
+++ components/catalog/KPSKProductPage.tsx
@@ -764,12 +764,59 @@ export default function KPSKProductPage({ product }) {
             <li>• на основании договора с частичной или полной постоплатой</li>
           </ul>
         </div>
+
         <div>
           <ProductParagraph>Первичная документация:</ProductParagraph>
           <ul className="text-[17px]">
             <li>• универсальный передаточный документ УПД</li>
             <li>• обмен документами через ЭДО</li>
           </ul>
+        </div>
+
+        <div className="w-full overflow-auto">
+          <table className="w-full min-w-231">
+            <thead>
+              <tr>
+                <th colSpan={4} className="uppercase">
+                  Банковские реквизиты ООО «Т.С.Т.»
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>ИНН</th>
+                <th>КПП</th>
+                <td>5404002676</td>
+                <td>540401001</td>
+              </tr>
+              <tr>
+                <th colSpan={2}>БИК</th>
+                <td>044525411</td>
+                <td>043207612</td>
+              </tr>
+              <tr>
+                <th colSpan={2} className="uppercase">
+                  Расчетный счет
+                </th>
+                <td>407 028 105 1307 00 000 31</td>
+                <td>407 028 100 2621 01 023 57</td>
+              </tr>
+              <tr>
+                <th colSpan={2} className="uppercase">
+                  Банк
+                </th>
+                <td>Филиал «Центральный» Банка ВТБ ПАО г. Москва</td>
+                <td>Кемеровское отделение № 8615 ПАО Сбербанк г. Кемерово</td>
+              </tr>
+              <tr>
+                <th colSpan={2} className="uppercase">
+                  Корреспондентский счет
+                </th>
+                <td>301 018 101 4525 00 004 11</td>
+                <td>301 018 102 0000 00 006 12</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
         <div>
