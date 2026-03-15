ea8ae6d9987c6119e258fc8d81f58bfb88004006
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-15 12:36:44 +0400

    accept html string

--- components/legacyHtml.tsx
+++ components/legacyHtml.tsx
@@ -1,27 +1,15 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function LegacyHtml({
-  path,
+  html,
   className = "",
 }: {
-  path: string;
+  html: string;
   className?: string;
 }) {
-  const [html, setHtml] = useState("");
   const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!path.startsWith("/legacy/")) {
-      console.error("Invalid legacy HTML path");
-      return;
-    }
-
-    fetch(path)
-      .then((res) => res.text())
-      .then((text) => setHtml(text));
-  }, [path]);
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -31,14 +19,12 @@ export default function LegacyHtml({
     scripts.forEach((script) => {
       const newScript = document.createElement("script");
 
-      // копируем атрибуты
       Array.from(script.attributes).forEach((attr) => {
         newScript.setAttribute(attr.name, attr.value);
       });
 
       newScript.textContent = script.textContent;
 
-      // заменяем старый script
       script.parentNode?.replaceChild(newScript, script);
     });
   }, [html]);
