ba2570e405c0534749f44b96cf2c7503a42a1fc8
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-16 18:46:59 +0400

    draft of tehnicheskaya-stranica

--- app/tehnicheskaya-stranica/page.tsx
+++ app/tehnicheskaya-stranica/page.tsx
@@ -6,6 +6,107 @@ export default function TehnicheskayaStranica() {
   return (
     <main className="3xl:px-0 mx-auto w-full max-w-336 space-y-6 px-2 pt-6 pb-14 sm:pt-14 lg:px-6 xl:px-10">
       <TehnicheskayaSearchForm />
+      <div className="space-y-3">
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов. Назначение калориферов. Конструкция калориферов.
+            Применение калориферов. Типы и виды калориферов. Калориферы для
+            низких температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+        <div className="flex gap-4 border px-4 py-3 text-[15px]">
+          <img src="/img/search/kalorifer_model.png" className="h-16" />
+          <p>
+            <a className="text-primary-dark cursor-pointer">
+              Производство калориферов
+            </a>
+            . Назначение калориферов. Конструкция калориферов. Применение
+            калориферов. Типы и виды калориферов. Калориферы для низких
+            температурных режимов. Калориферы водяные и паровые для сушки
+            материалов.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
