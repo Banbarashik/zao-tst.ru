8bb4f52c0f562c619d79d9d9152be253604d8130
Banbarashik <odinokyskitalec@yahoo.com>
2026-03-19 12:29:38 +0400

    add new menu btn

--- components/HeaderWithSearch.tsx
+++ components/HeaderWithSearch.tsx
@@ -296,7 +296,6 @@ export default function HeaderWithSearch(): JSX.Element {
           <NavigationMenu />
           <ContactFormTrigger
             triggerBtnVariant="default"
-            triggerBtnSize="lg"
             triggerBtnClassName="h-12 px-3 cursor-pointer hover:bg-primary-dark text-base font-bold xl:text-xl border border-[#A5A5A5]"
             amountClassName="bg-accent absolute text-black right-0 bottom-0 inline-flex size-6 translate-x-1/2 translate-y-1/2 items-center justify-center rounded-full text-sm font-semibold"
           />
