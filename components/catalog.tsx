214916f34df41f72509f5c7c2079b0a1e9767cb0
Banbarashik <odinokyskitalec@yahoo.com>
2026-05-09 16:47:05 +0400

    small change to home page catalog btns

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -137,13 +137,14 @@ export default function Catalog() {
                   >
                     <Link
                       href={product.url}
-                      className="hover:text-primary bg-card text-card-foreground flex h-full flex-col items-center gap-4 rounded-xl border px-2 pt-5 pb-5 text-center shadow-sm sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:h-auto lg:px-4 lg:text-sm xl:px-10 2xl:text-base"
+                      className="hover:text-primary bg-card text-card-foreground flex h-full flex-col items-center gap-4 rounded-xl border px-2 pt-7 pb-6 text-center shadow-sm sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:h-auto lg:px-4 lg:text-sm xl:px-10 2xl:text-base"
                     >
                       <Image
                         src={product.img}
                         alt={product.name}
                         width={750}
                         height={750}
+                        className="px-6"
                       />
                       <p className="font-bold tracking-wide uppercase">
                         {product.name}
