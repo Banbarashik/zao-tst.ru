4e1c2be2636ec9512087289c25a1c28a3adf71e8
Banbarashik <odinokyskitalec@yahoo.com>
2026-05-20 13:35:34 +0400

    clean up

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -164,11 +164,11 @@ export default function Catalog() {
         <h2 className="text-base font-bold uppercase sm:text-lg md:text-xl lg:text-[22px] xl:text-2xl">
           Калориферы и отопительные агрегаты воздушного отопления
         </h2>
-        <ul className="mb-6 flex gap-2 sm:mt-6 sm:gap-2 md:gap-4 lg:gap-10 xl:gap-12">
+        <ul className="my-6 flex gap-2 md:gap-4 lg:gap-10 xl:gap-12">
           <li className="w-full">
             <Link
               href="#"
-              className="hover:text-primary bg-card text-card-foreground relative flex aspect-16/10 h-full w-full flex-col items-center gap-4 rounded-xl border px-2 pt-7 pb-6 text-center shadow-sm sm:aspect-auto sm:text-[13px] md:aspect-16/10 md:px-4 md:text-center md:text-[13px] lg:h-auto lg:px-4 lg:text-sm xl:px-10 2xl:text-base"
+              className="hover:text-primary bg-card text-card-foreground relative flex aspect-16/10 h-full w-full flex-col items-center gap-4 rounded-xl border px-2 pt-7 pb-6 text-center shadow-sm sm:aspect-auto sm:text-[13px] md:aspect-16/10 md:px-4 md:text-center md:text-[13px] lg:h-auto lg:text-sm xl:px-10 2xl:text-base"
             >
               <div className="relative aspect-16/10 h-full">
                 <Image src="/img/KPVS_KPVU_SFO.png" alt="" fill />
@@ -178,7 +178,7 @@ export default function Catalog() {
               </p>
             </Link>
           </li>
-          <li className="bg-card text-card-foreground hidden basis-full flex-col items-center gap-4 rounded-xl border px-2 pt-7 pb-6 text-center shadow-sm sm:flex sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:px-4 lg:text-sm xl:px-10 2xl:text-base">
+          <li className="bg-card text-card-foreground hidden w-full basis-full flex-col items-center gap-4 rounded-xl border px-2 pt-7 pb-6 text-center shadow-sm sm:flex sm:w-[240px] sm:text-[13px] md:w-full md:px-4 lg:text-sm xl:px-10 2xl:text-base">
             <ModelViewer modelUrl="/models/ksk-3_Shitok.glb" />
             <p className="font-bold tracking-wide uppercase">
               3D-модель калорифера КСк
