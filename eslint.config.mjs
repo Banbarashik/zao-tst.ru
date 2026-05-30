d13f22d0a34acff0bf6baeabe20f93172d18f22e
Banbarashik <odinokyskitalec@yahoo.com>
2026-05-30 19:02:08 +0400

    update eslint to match next 16 guide

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypescript,
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules",
-      "dist",
-      ".next",
-      "build",
-      "public",
-      "coverage",
-      "**/*.d.ts", // optional: ignore TypeScript declaration files
-    ],
-  },
-];
-
-export default eslintConfig;
+  globalIgnores([
+    "node_modules/**",
+    "dist/**",
+    ".next/**",
+    "build/**",
+    "public/**",
+    "coverage/**",
+    "**/*.d.ts",
+  ]),
+]);
