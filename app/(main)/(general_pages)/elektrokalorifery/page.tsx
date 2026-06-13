1c0661fedb1a82334f2493de0f2f66d3cfbe8ac7
Banbarashik <odinokyskitalec@yahoo.com>
2026-06-13 16:32:33 +0400

    PowerTable

--- app/(main)/(general_pages)/elektrokalorifery/page.tsx
+++ app/(main)/(general_pages)/elektrokalorifery/page.tsx
@@ -1,5 +1,6 @@
 import sfoWiringTablesData from "@/data/sfoWiringTables.json";
 
+import Link from "next/link";
 import Image from "next/image";
 
 import type { SfoWiringSchema } from "@/types/sfoWiringSchema";
@@ -9,188 +10,324 @@ import { SfoWiringTable } from "@/components/general_pages/SfoWiringTable";
 
 export default function ElektrokaloriferyPage() {
   return (
-    <ol className="space-y-12">
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-20/9 w-full">
-            <Image
-              src="/img/kalorifer_sfo-16.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-16"
-              title="Схема подключения фазных блоков электрокалорифера СФО-16"
-              fill
-            />
+    <>
+      <PowerTable />
+      {/* СХЕМЫ-ТАБЛИЦЫ */}
+      <ol className="space-y-12">
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-20/9 w-full">
+              <Image
+                src="/img/kalorifer_sfo-16.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-16"
+                title="Схема подключения фазных блоков электрокалорифера СФО-16"
+                fill
+              />
+            </div>
+            <div className="relative aspect-20/9 w-full">
+              <Image
+                src="/img/kalorifer_sfo-16_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-16"
+                title="Схема подключения нейтральной стороны калорифера СФО-16"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-20/9 w-full">
-            <Image
-              src="/img/kalorifer_sfo-16_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-16"
-              title="Схема подключения нейтральной стороны калорифера СФО-16"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-16">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-16"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-16">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-16"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-2/1 w-full">
-            <Image
-              src="/img/kalorifer_sfo-25.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-25"
-              title="Схема подключения фазных блоков электрокалорифера СФО-25"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-2/1 w-full">
+              <Image
+                src="/img/kalorifer_sfo-25.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-25"
+                title="Схема подключения фазных блоков электрокалорифера СФО-25"
+                fill
+              />
+            </div>
+            <div className="relative aspect-2/1 w-full">
+              <Image
+                src="/img/kalorifer_sfo-25_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-25"
+                title="Схема подключения нейтральной стороны калорифера СФО-25"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-2/1 w-full">
-            <Image
-              src="/img/kalorifer_sfo-25_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-25"
-              title="Схема подключения нейтральной стороны калорифера СФО-25"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-25">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-25"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-25">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-25"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-5/3 w-full">
-            <Image
-              src="/img/kalorifer_sfo-40.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-40"
-              title="Схема подключения фазных блоков электрокалорифера СФО-40"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-5/3 w-full">
+              <Image
+                src="/img/kalorifer_sfo-40.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-40"
+                title="Схема подключения фазных блоков электрокалорифера СФО-40"
+                fill
+              />
+            </div>
+            <div className="relative aspect-5/3 w-full">
+              <Image
+                src="/img/kalorifer_sfo-40_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-40"
+                title="Схема подключения нейтральной стороны калорифера СФО-40"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-5/3 w-full">
-            <Image
-              src="/img/kalorifer_sfo-40_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-40"
-              title="Схема подключения нейтральной стороны калорифера СФО-40"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-40">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-40"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-40">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-40"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-20/13 w-full">
-            <Image
-              src="/img/kalorifer_sfo-60.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-60"
-              title="Схема подключения фазных блоков электрокалорифера СФО-60"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-20/13 w-full">
+              <Image
+                src="/img/kalorifer_sfo-60.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-60"
+                title="Схема подключения фазных блоков электрокалорифера СФО-60"
+                fill
+              />
+            </div>
+            <div className="relative aspect-20/13 w-full">
+              <Image
+                src="/img/kalorifer_sfo-60_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-60"
+                title="Схема подключения нейтральной стороны калорифера СФО-60"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-20/13 w-full">
-            <Image
-              src="/img/kalorifer_sfo-60_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-60"
-              title="Схема подключения нейтральной стороны калорифера СФО-60"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-60">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-60"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-60">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-60"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-10/7 w-full">
-            <Image
-              src="/img/kalorifer_sfo-100.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-100"
-              title="Схема подключения фазных блоков электрокалорифера СФО-100"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-10/7 w-full">
+              <Image
+                src="/img/kalorifer_sfo-100.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-100"
+                title="Схема подключения фазных блоков электрокалорифера СФО-100"
+                fill
+              />
+            </div>
+            <div className="relative aspect-10/7 w-full">
+              <Image
+                src="/img/kalorifer_sfo-100_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-100"
+                title="Схема подключения нейтральной стороны калорифера СФО-100"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-10/7 w-full">
-            <Image
-              src="/img/kalorifer_sfo-100_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-100"
-              title="Схема подключения нейтральной стороны калорифера СФО-100"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-100">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-100"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-100">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-100"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-4/3 w-full">
-            <Image
-              src="/img/kalorifer_sfo-160.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-160"
-              title="Схема подключения фазных блоков электрокалорифера СФО-160"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-4/3 w-full">
+              <Image
+                src="/img/kalorifer_sfo-160.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-160"
+                title="Схема подключения фазных блоков электрокалорифера СФО-160"
+                fill
+              />
+            </div>
+            <div className="relative aspect-4/3 w-full">
+              <Image
+                src="/img/kalorifer_sfo-160_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-160"
+                title="Схема подключения нейтральной стороны калорифера СФО-160"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-4/3 w-full">
-            <Image
-              src="/img/kalorifer_sfo-160_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-160"
-              title="Схема подключения нейтральной стороны калорифера СФО-160"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-160">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-160"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-160">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-160"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
+          </Spoiler>
+        </li>
 
-      <li className="space-y-4">
-        <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
-          <div className="relative aspect-10/9 w-full">
-            <Image
-              src="/img/kalorifer_sfo-250.png"
-              alt="Подключение фазных термостойких проводов в коробе СФО-250"
-              title="Схема подключения фазных блоков электрокалорифера СФО-250"
-              fill
-            />
+        <li className="space-y-4">
+          <div className="flex w-full flex-col gap-3 sm:flex-row sm:gap-0">
+            <div className="relative aspect-10/9 w-full">
+              <Image
+                src="/img/kalorifer_sfo-250.png"
+                alt="Подключение фазных термостойких проводов в коробе СФО-250"
+                title="Схема подключения фазных блоков электрокалорифера СФО-250"
+                fill
+              />
+            </div>
+            <div className="relative aspect-10/9 w-full">
+              <Image
+                src="/img/kalorifer_sfo-250_electro.png"
+                alt="Подключение нулевого провода к шине ТЭНов СФО-250"
+                title="Схема подключения нейтральной стороны калорифера СФО-250"
+                fill
+              />
+            </div>
           </div>
-          <div className="relative aspect-10/9 w-full">
-            <Image
-              src="/img/kalorifer_sfo-250_electro.png"
-              alt="Подключение нулевого провода к шине ТЭНов СФО-250"
-              title="Схема подключения нейтральной стороны калорифера СФО-250"
-              fill
+          <Spoiler title="Принципиально-монтажная схема СФО-250">
+            <SfoWiringTable
+              schema={sfoWiringTablesData["СФО-250"] as SfoWiringSchema}
             />
-          </div>
-        </div>
-        <Spoiler title="Принципиально-монтажная схема СФО-250">
-          <SfoWiringTable
-            schema={sfoWiringTablesData["СФО-250"] as SfoWiringSchema}
-          />
-        </Spoiler>
-      </li>
-    </ol>
+          </Spoiler>
+        </li>
+      </ol>
+    </>
+  );
+}
+
+const models = [
+  {
+    model: "СФО-16",
+    url: "/elektrokalorifer-sfo-16",
+    power: { total: 15, section: 7.5, block: 2.5 },
+    current: { line: 23, section: 11, block: 4 },
+    cable: { main: "4", section: 2.5, block: 2.5 },
+  },
+  {
+    model: "СФО-25",
+    url: "/elektrokalorifer-sfo-25",
+    power: { total: 22.5, section: 7.5, block: 2.5 },
+    current: { line: 34, section: 11, block: 4 },
+    cable: { main: "6", section: 2.5, block: 2.5 },
+  },
+  {
+    model: "СФО-40",
+    url: "/elektrokalorifer-sfo-40",
+    power: { total: 45, section: 15, block: 5 },
+    current: { line: 68, section: 23, block: 8 },
+    cable: { main: "16", section: 4, block: 2.5 },
+  },
+  {
+    model: "СФО-60",
+    url: "/elektrokalorifer-sfo-60",
+    power: { total: 67.5, section: 22.5, block: 7.5 },
+    current: { line: 103, section: 34, block: 11 },
+    cable: { main: "35", section: 6, block: 2.5 },
+  },
+  {
+    model: "СФО-100",
+    url: "/elektrokalorifer-sfo-100",
+    power: { total: 90, section: 30, block: 10 },
+    current: { line: 137, section: 46, block: 15 },
+    cable: { main: "50", section: 10, block: 4 },
+  },
+  {
+    model: "СФО-160",
+    url: "/elektrokalorifer-sfo-160",
+    power: { total: 157.5, section: 52.5, block: 17.5 },
+    current: { line: 240, section: 80, block: 27 },
+    cable: { main: "95 2×50", section: 25, block: 10 },
+  },
+  {
+    model: "СФО-250",
+    url: "/elektrokalorifer-sfo-250",
+    power: { total: 247.5, section: 82.5, block: 27.5 },
+    current: { line: 376, section: 125, block: 42 },
+    cable: { main: "185 2×95", section: 50, block: 16 },
+  },
+];
+
+const headerGroups = [
+  { label: "Мощность, кВт", cols: ["общая", "секция", "блок"] },
+  { label: "Расчетный ток, А", cols: ["линия", "секция", "блок"] },
+  {
+    label: (
+      <>
+        Мин. сечение кабеля, мм<sup>2</sup>
+      </>
+    ),
+    cols: ["магистраль", "секция", "блок"],
+  },
+];
+
+function PowerTable() {
+  return (
+    <div className="w-full overflow-x-auto">
+      <table className="w-full min-w-231 table-fixed xl:min-w-auto">
+        <caption className="border border-b-0 border-[#723910] py-1 uppercase">
+          Параметры силовых линий, мощность и сечения проводников
+          электрокалориферов СФО
+        </caption>
+        {
+          <colgroup>
+            <col className="w-28" />
+            {Array.from({ length: 9 }).map((_, i) => (
+              <col key={i} />
+            ))}
+          </colgroup>
+        }
+
+        <thead>
+          <tr className="uppercase">
+            <th rowSpan={2}>Модель</th>
+            {headerGroups.map((g) => (
+              <th key={g.label} colSpan={3} className="py-1">
+                {g.label}
+              </th>
+            ))}
+          </tr>
+          <tr>
+            {headerGroups.flatMap((g) =>
+              g.cols.map((col) => (
+                <th key={`${g.label}-${col}`} className="py-1.5">
+                  {col}
+                </th>
+              )),
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {models.map((row) => (
+            <tr key={row.model}>
+              <td className="px-1.5 py-1 text-left">
+                <Link
+                  href={row.url}
+                  className="text-primary-darker hover:text-primary-dark"
+                >
+                  {row.model}
+                </Link>
+              </td>
+              {/* Power */}
+              <td>{row.power.total}</td>
+              <td>{row.power.section}</td>
+              <td>{row.power.block}</td>
+              {/* Current */}
+              <td>{row.current.line}</td>
+              <td>{row.current.section}</td>
+              <td>{row.current.block}</td>
+              {/* Cable */}
+              <td>{row.cable.main}</td>
+              <td>{row.cable.section}</td>
+              <td>{row.cable.block}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
