7a7f63974e4aabdca3e1d72ea523e51ea5a4f468
Banbarashik <odinokyskitalec@yahoo.com>
2026-07-15 14:29:22 +0400

    add sections common across all sfotc pages

--- app/(main)/(products)/ustanovka-sfotc-250/page.tsx
+++ app/(main)/(products)/ustanovka-sfotc-250/page.tsx
@@ -1,15 +1,25 @@
+import productData from "@/data/products.json";
 import { sfotcOperatingModes } from "@/data/sfotcOperatingModes";
 
+import { ElectroProductOverviewSection } from "@/components/catalog/electro/ElectroProductOverviewSection";
+import { ElectroSpecsSection } from "@/components/catalog/electro/ElectroSpecsSection";
 import { SfotcOperatingModeTable } from "@/components/general_pages/SfotcOperatingModeTable";
+import { SFOTCDimensionsSection } from "@/components/catalog/electro/SFOTCDimensionsSection";
+
+const product = productData.find((p) => p.id === "ustanovka-sfotc-250");
 
 export default function SFOTC250Page() {
+  if (!product) return;
+
   const table = sfotcOperatingModes.find(
     (mode) => mode.id === "sfotc-250-17000",
   )!;
 
   return (
     <div className="@container w-full lg:overflow-x-auto">
-      <SfotcOperatingModeTable table={table} />
+      {/* <ElectroProductOverviewSection product={product} />
+      <ElectroSpecsSection product={product} />
+      <SFOTCDimensionsSection product={product} /> */}
     </div>
   );
 }
