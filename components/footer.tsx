25622f56753dade4b5c99dfbb9204e9b3e402a17
Banbarashik <odinokyskitalec@yahoo.com>
2026-08-29 14:23:17 +0400

    start rebuilding footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,138 +1,137 @@
 import Link from "next/link";
 
-import { Mail, MapPinned, Smartphone } from "lucide-react";
+import {
+  Cog,
+  Factory,
+  Mail,
+  MapPinned,
+  Phone,
+  Smartphone,
+  Wrench,
+} from "lucide-react";
 import { GithubIcon } from "@/components/icons/github";
 
 import Logo from "@/components/ui/logo";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer
       id="footer"
-      className="bg-secondary text-secondary-foreground 4xl:justify-center relative py-4 pr-2 sm:pr-4 sm:pb-3.5 sm:pl-30 lg:flex lg:h-62 lg:items-center lg:gap-16 lg:pr-10 lg:pl-0 xl:h-auto xl:p-10"
+      className="relative h-76 overflow-hidden bg-[#303030] py-5 text-white"
     >
-      <div className="4xl:justify-start 4xl:gap-20 3xl:gap-10 4xl:w-auto lg:flex lg:w-full lg:items-center lg:justify-between lg:gap-12 xl:ml-0">
-        <div className="w-26 xl:hidden" />
-        <div className="mb-6 flex items-center justify-center gap-4 sm:justify-start lg:order-last lg:m-0">
-          <Logo place="footer" />
-          <div className="text-lg">
-            <p className="text-accent">Электронная почта</p>
-            <a
-              href="mailto:zao_tst@mail.ru"
-              className="text-accent flex items-center gap-2 hover:text-white"
-            >
-              <Mail size={20} />
-              zao_tst@mail.ru
-            </a>
-          </div>
-        </div>
-
-        <div className="flex flex-col justify-end gap-2.5 text-right sm:hidden">
-          <p>
-            <span className="mb-2 flex justify-end gap-2">
-              <MapPinned /> <span>Адрес:</span>
-            </span>
-            <span className="block">Кемеровская область,</span>
-            <span className="block">г. Киселевск, ул. Юргинская,1</span>
-          </p>
-
-          <div>
-            <div className="mb-2 flex justify-end gap-2">
-              <Smartphone />
-              Телефоны:
+      {/* <Image
+        src="/img/kalorifer.svg"
+        alt=""
+        aria-hidden="true"
+        width={180}
+        height={164}
+        className="pointer-events-none absolute top-1/5 right-10 opacity-20"
+      /> */}
+      <div className="max-w-8xl mx-auto">
+        <div className="flex justify-between">
+          {/* 1-й столбик */}
+          <div className="flex max-w-120 flex-col gap-4">
+            <div className="flex items-center gap-3 font-serif text-[18px] leading-[1.3]">
+              <Image
+                src="/img/logo_header.png"
+                alt=""
+                width={54}
+                height={54}
+                className="opacity-90"
+              />
+              <p className="text-accent">
+                Завод по производству воздушно-отопительного оборудования ООО
+                «Т.С.Т.»
+              </p>
             </div>
-            <div>Технические вопросы</div>
-            <div className="font-semibold">8-961-737-83-14</div>
-            <div>Отдел продаж</div>
-            <div className="font-semibold">8-904-968-14-88</div>
+            <p className="text-xs">
+              Наш центр объединил в себе все этапы создания современного
+              промышленного инструмента: разработку, проектирование, создание
+              макетных образцов, проведение лабораторных испытаний и
+              исследований, выпуск опытных образцов, проведение натурных
+              испытаний и производство.
+            </p>
+          </div>
+
+          {/* 2-й столбик */}
+          <div className="space-y-6">
+            <p className="text-accent border-b-2 border-white/10 pb-4 text-xl font-bold">
+              Каталог оборудования
+            </p>
+            <ul className="space-y-2">
+              <li>Водяные калориферы</li>
+              <li>Паровые калориферы</li>
+              <li>Электрические калориферы</li>
+              <li>Блок расчета и подбора</li>
+            </ul>
+          </div>
+
+          {/* 3-й столбик */}
+          <div className="space-y-6">
+            <p className="text-accent border-b-2 border-white/10 pb-4 text-xl font-bold">
+              Контакты предприятия
+            </p>
+            <address className="not-italic">
+              <ul className="space-y-2">
+                <li className="flex items-center gap-2">
+                  <div className="w-min rounded-full bg-white p-1.25">
+                    <Phone fill="#303030" width={16} height={16} />
+                  </div>
+                  <p>8-961-737-83-14</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <div className="w-min rounded-full bg-white p-1.25">
+                    <Phone fill="#303030" width={16} height={16} />
+                  </div>
+                  <p>8-904-968-14-88</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <div className="w-min rounded-full bg-white p-1.25">
+                    <Mail stroke="#303030" width={16} height={16} />
+                  </div>
+                  <a href="mailto:zao_tst@mail.ru">zao_tst@mail.ru</a>
+                </li>
+                <li className="flex items-center gap-2">
+                  <div className="w-min rounded-full bg-white p-0.75">
+                    <Factory fill="#303030" width={20} height={20} />
+                  </div>
+                  <p>Кемеровская область, г. Киселевск, ул. Юргинская, 1</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <div className="w-min rounded-full bg-white p-0.75">
+                    <Cog
+                      width={20}
+                      height={20}
+                      strokeWidth={2.5}
+                      stroke="#303030"
+                    />
+                  </div>
+                  <p>Время работы: пн.-пт. 8:00-17.00; сб.-вс. – выходной</p>
+                </li>
+              </ul>
+            </address>
           </div>
         </div>
 
-        <div className="3xl:hidden hidden space-y-3 sm:block">
-          <p>
-            <span className="mb-2 flex gap-2">
-              <MapPinned /> <span>Адрес предприятия:</span>
-            </span>
-            <span className="block">
-              Кемеровская область, г. Киселевск, ул. Юргинская, 1
-            </span>
-          </p>
-
-          <div className="mb-2 hidden gap-1 sm:flex xl:hidden">
-            <Smartphone />
-            Телефоны:
-          </div>
-          <ul className="space-y-1.5">
-            <li className="flex gap-2">
-              <Smartphone className="hidden xl:block" /> 8-961-737-83-14 -
-              технические вопросы
-            </li>
-            <li className="flex gap-2">
-              <Smartphone className="hidden xl:block" /> 8-904-968-14-88 - отдел
-              продаж
-            </li>
-          </ul>
-        </div>
-
-        <p className="3xl:block hidden font-medium xl:text-sm 2xl:text-base">
-          <span className="mb-2 flex gap-2">
-            <MapPinned /> <span>Адрес предприятия:</span>
-          </span>
-          <span className="block">
-            Кемеровская область, г. Киселевск, ул. Юргинская, 1
-          </span>
-        </p>
-        <div className="3xl:block hidden xl:text-sm 2xl:text-base">
-          <div className="mb-1 flex gap-1">
-            <Smartphone />
-            Телефоны:
-          </div>
-          <ul>
-            <li>
-              <span>8-961-737-83-14</span> - технические вопросы
-            </li>
-            <li>
-              <span>8-904-968-14-88</span> - отдел продаж
-            </li>
-          </ul>
-        </div>
-
-        <div className="order-first hidden space-y-1 text-[15px] xl:block">
-          <p className="mb-2">Разработка сайта:</p>
+        <p className="relative flex items-center gap-3">
+          Разработка сайта:{" "}
           <Link
             href="https://github.com/Banbarashik"
             target="_blank"
-            className="text-accent flex items-center gap-2 opacity-70 hover:opacity-100"
+            className="text-accent flex items-center gap-2"
           >
             <GithubIcon />
             Banbarashik
           </Link>
           <a
             href="mailto:odinokiyskitalec@gmail.com"
-            className="text-accent flex items-center gap-2 opacity-70 hover:opacity-100"
+            className="text-accent flex items-center gap-2"
           >
             <Mail size={20} />
             odinokiyskitalec@gmail.com
           </a>
-        </div>
-      </div>
-      <div className="absolute bottom-23 -left-15 -rotate-90 space-y-1 text-[15px] lg:bottom-22 xl:hidden">
-        <p>Разработка сайта:</p>
-        <Link
-          href="https://github.com/Banbarashik"
-          target="_blank"
-          className="text-accent flex items-center gap-2"
-        >
-          <GithubIcon />
-          Banbarashik
-        </Link>
-        <a
-          href="mailto:odinokiyskitalec@gmail.com"
-          className="text-accent flex items-center gap-2"
-        >
-          <Mail size={20} />
-          odinokiyskitalec@gmail.com
-        </a>
+        </p>
       </div>
     </footer>
   );
